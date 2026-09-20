d5977b8e5b8c07659ebba8105df908c8fd393bee
Xusrav0 <xusravshohidjonov@gmail.com>
2026-09-20 23:27:35 +0900

    BRR: initial setup

---
+++ prectisce.ts
@@ -0,0 +1,54 @@
+// Compiled Languages: Java, GoLang, C, C++, C#, Rust  => Compiling(mashina tiliga o'giriladi) keyin Running bo'ladi 
+
+// Interpreted Languages: NodeJS, Python, PHP, Ruby    =>  Running
+
+
+// Error (Java) compile error beradi run bo'lishdan oldin
+
+// Error Type
+
+// Primitive type
+let box: string ='hello';
+box = "100"; // 100 number bo'lganda error beradi 
+
+let counter: number = 100;
+
+let stage: number | string = 'hello';
+
+let pending: boolean = true;
+
+// Object type
+// interface
+interface Person {
+    name: string,
+    age: number,
+    nation: string
+}
+
+let person: Person = {
+    name: 'Aaron',
+    age: 30, // number berganimiz uchun 'hi' va stringlar ishlamidi
+    nation: 'American'
+}
+
+let skills: (number | string)[]; // ham number va string qilish uchun
+skills = ['Problem Solving', 'software Design', 'Programming', 100]
+
+class Person1 {
+    age: number;
+    firstName: string;
+    lastName: string;
+
+    constructor(age: number, firstName: string, lastName: string) {
+        this.age = age;
+        this.firstName = firstName;
+        this.lastName = lastName
+    }
+}
+
+const person1 = new Person1(30, "Aaron", 'Robertson');
+
+
+
+
+
