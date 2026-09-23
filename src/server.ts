7712403289c3cc48223e191ad75e9ae7f2ff69f1
Xusrav0 <xusravshohidjonov@gmail.com>
2026-09-23 17:40:19 +0900

    feat:  express integrations

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 // import moment from 'moment'  // <= bu modul js,  // common js const moment = require('moment')
 import dotenv from 'dotenv'
 dotenv.config();
-
 import mongoose from 'mongoose'
+import app from './app'
 
 mongoose
 .connect(process.env.MONGO_URL as string, {})
 .then(data => {
     console.log('MongoDB conncetion succeed')
     const PORT = process.env.PORT ?? 3003;
+    app.listen(PORT, function() {
+        console.log(`The server is running successfully on port: ${PORT}`)
+    })
 })
 .catch(err => console.log('Error on connection MongoDB', err));
