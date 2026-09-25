ebf1b795c07f61dc95b0831415d979766c1e10aa
Xusrav0 <xusravshohidjonov@gmail.com>
2026-09-25 18:31:29 +0900

    feat: Completed TASK N

--- src/train.ts
+++ src/train.ts
@@ -1,4 +1,35 @@
 /*
+TASK N:
+Shunday function yozing, u string qabul qilsin va string 
+palindrom yani togri oqilganda ham, orqasidan oqilganda 
+ham bir hil oqiladigan soz ekanligini aniqlab boolean qiymat qaytarsin.
+MASALAN: palindromCheck("dad") return true;  palindromCheck("son") return false;
+*/
+
+function back(str: any) {
+    let reversed = '';
+    for (let char of str) {
+        reversed = char + reversed
+    }
+    return str === reversed
+}
+
+console.log(back("dad"));
+console.log(back("MERN"));
+/*
+// function palindromCheck(str: string) {
+//     return str === str.split('').reverse().join('')
+    
+// }
+// console.log(palindromCheck("dad"));
+// console.log(palindromCheck("son"));
+// console.log(palindromCheck("MERN"));
+// console.log(palindromCheck("level"));
+
+
+
+
+/*
 TASK M: 
 
 Shunday function yozing, u raqamlardan tashkil topgan 
@@ -9,6 +40,7 @@ MASALAN: getSquareNumbers([1, 2, 3]) return [{number: 1, square: 1},
 {number: 2, square: 4}, {number: 3, square: 9}];
 
 */
+/*
 interface SquareNumbers {
     number: number;
     square: number;
@@ -29,9 +61,7 @@ function getSquareNumbers(raqam: number[]): SquareNumbers[] {
 
 console.log(getSquareNumbers([1, 2, 3]));
 // console.log(getSquareNumbers([8, 7, 6]));
-
-
-
+*/
 
 
 
